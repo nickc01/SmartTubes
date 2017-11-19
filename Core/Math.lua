bcb622f7914ca06469dc2bb63a88fba7cfeac06c
nrcullen98 <nickc01@outlook.com>
2017-11-18 20:13:45 -0600

    Added Content to ElementBase.lua

---
+++ Core/Math.lua
@@ -0,0 +1,23 @@
+vec = {};
+local vec = vec;
+rect = {};
+local rect = rect;
+
+function vec.add(A,B)
+	return {A[1] + B[1],A[2] + B[2]};
+end
+
+function vec.sub(A,B)
+	return {A[1] - B[1],A[2] - B[2]};
+end
+
+function rect.vecAdd(A,B)
+	return {A[1] + B[1],A[2] + B[2],A[3] + B[1],A[4] + B[2]};
+end
+
+function rect.vecSub(A,B)
+	return {A[1] - B[1],A[2] - B[2],A[3] - B[1],A[4] - B[2]};
+end
+function rect.minimize(A)
+	return {0,0,A[3] - A[1],A[4] - A[2]};
+end
