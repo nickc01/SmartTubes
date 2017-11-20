d5d54e9d646bbaf32564d2848f73b7a68546daa4
nrcullen98 <nickc01@outlook.com>
2017-11-19 20:45:38 -0600

    Clean Up

--- Core/CanvasCore/ElementBase.lua
+++ Core/CanvasCore/ElementBase.lua
@@ -10,6 +10,26 @@ function CreateElement(CanvasName)
 	local Position;
 	local ID;
 
+	local CalculatePosition = function()
+		local LowestPoint;
+		for k,i in pairs(Drawables) do
+			if LowestPoint == nil then
+				LowestPoint = {i.Rect[1],i.Rect[2]};
+			else
+				if i.Rect[1] < LowestPoint[1] then
+					LowestPoint[1] = i.Rect[1];
+				end
+				if i.Rect[2] < LowestPoint[2] then
+					LowestPoint[2] = i.Rect[2];
+				end
+			end
+		end
+		if LowestPoint == nil then
+			LowestPoint = {0,0};
+		end
+		Position = LowestPoint;
+	end
+
 	local Controller = setmetatable({},{__index = function()
 		error("This Element hasn't been finished yet");
 	end});
@@ -24,7 +44,7 @@ function CreateElement(CanvasName)
 		for k,i in pairs(Drawables) do
 			i.Rect = rect.vecAdd(i.Rect,Diff);
 		end
-		Position = vec.Add(Position,Diff);
+		Position = vec.add(Position,Diff);
 	end
 
 	ControllerBase.SetAbsolutePosition = function(Pos)
@@ -43,6 +63,9 @@ function CreateElement(CanvasName)
 	ControllerBase.GetPosition = function()
 		return {Position[1],Position[2]};
 	end
+	Element.GetPosition = function()
+		return Position;
+	end
 
 	ControllerBase.GetAbsolutePosition = function()
 		if Element.Parent ~= nil then
@@ -52,26 +75,37 @@ function CreateElement(CanvasName)
 	end
 
 	Element.AddDrawable = function(Name,Rect,Image,IsTiled)
-		Drawables[Name] = {
+		Drawables[#Drawables + 1] = {
+			Name = Name,
 			Rect = Rect,
 			Image = Image,
 			IsTiled = IsTiled,
 			TextureRect = rect.minimize(Rect)
 		}
+		CalculatePosition();
 	end
 
 	Element.SetDrawableRect = function(Name,Rect)
-		if Drawables[Name] == nil then
-			error("Drawable of " .. sb.print(Name) .. " doesn't exist");
+		--sb.logInfo("Drawables = " .. sb.printJson(Drawables,1));
+		for k,i in ipairs(Drawables) do
+			--sb.logInfo("i.Name = " .. sb.print(i.Name));
+			--sb.logInfo("Name = " .. sb.print(Name));
+			if i.Name == Name then
+				Drawables[k].Rect = rect.vecAdd(Rect,Position);
+				return nil;
+			end
 		end
-		Drawables[Name].Rect = Rect;
+		error("Drawable of " .. sb.print(Name) .. " doesn't exist");
+		
 	end
 
 	Element.GetDrawableRect = function(Name)
-		if Drawables[Name] == nil then
-			error("Drawable of " .. sb.print(Name) .. " doesn't exist");
+		for k,i in ipairs(Drawables) do
+			if i.Name == Name then
+				return rect.vecSub(i.Rect,Position);
+			end
 		end
-		return Drawables[Name].Rect;
+		error("Drawable of " .. sb.print(Name) .. " doesn't exist");
 	end
 
 	Element.Draw = function()
@@ -91,32 +125,24 @@ function CreateElement(CanvasName)
 	Element.SetCanvas = function(AliasName)
 		Canvas = CanvasCore.GetCanvas(AliasName);
 	end
+	Element.GetCanvas = function()
+		return Canvas;
+	end
 
 	Element.AddControllerValue = function(name,value)
 		ControllerBase[name] = value;
 	end
+	
+	ControllerBase.Delete = function()
+		
+	end
+	
 
 	Element.Finish = function()
 		if Canvas == nil then
 			error("This element doesn't have a Canvas Alias Set, run SetCanvas() to set the element's Canvas Alias");
 		end
-		local LowestPoint;
-		for k,i in pairs(Drawables) do
-			if LowestPoint == nil then
-				LowestPoint = {i.Rect[1],i.Rect[2]};
-			else
-				if i.Rect[1] < LowestPoint[1] then
-					LowestPoint[1] = i.Rect[1];
-				end
-				if i.Rect[2] < LowestPoint[2] then
-					LowestPoint[2] = i.Rect[2];
-				end
-			end
-		end
-		if LowestPoint == nil then
-			LowestPoint = {0,0};
-		end
-		Position = LowestPoint;
+		CalculatePosition();
 		Controller = ControllerBase;
 		ID = sb.makeUuid();
 		return Controller;
