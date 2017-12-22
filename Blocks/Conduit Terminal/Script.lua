7c8903ba0cacd33f5b73d2a63a8a8da4750123da
nrcullen98 <nickc01@outlook.com>
2017-12-21 22:09:35 -0600

    Finished Fluid Conduit and Starting Development on Conduit Terminal

---
+++ Blocks/Conduit Terminal/Script.lua
@@ -0,0 +1,40 @@
+local Hue = 0;
+local Saturation = 0;
+
+local UpdateLooks;
+
+function init()
+	Hue = config.getParameter("Hue",0);
+	Saturation = config.getParameter("Saturation",0);
+	message.setHandler("SetHue",function(_,_,newHue)
+		Hue = newHue;
+		object.setConfigParameter("Hue",newHue);
+		UpdateLooks();
+	end);
+	message.setHandler("SetSaturation",function(_,_,newSat)
+		Saturation = newSat;
+		object.setConfigParameter("Saturation",newSat);
+		UpdateLooks();
+	end);
+	UpdateLooks();
+end
+
+UpdateLooks = function()
+	object.setProcessingDirectives("?hueshift=" .. Hue .. "?saturation=" .. Saturation);
+end
+
+function update(dt)
+	Hue = Hue + 1;
+	if Hue > 360 then
+		Hue = 0;
+	end
+	object.setProcessingDirectives("?hueshift=" .. Hue);
+end
+
+function die()
+	
+end
+
+function uninit()
+	
+end
